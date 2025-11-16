0d786b5e6d6c6dc986fe7830391e9eb68e013175
Italo Williams <italowilliams2020@gmail.com>
2025-11-16 11:26:25 -0300

    added vercel config

--- pixelaria-website/src/main.tsx
+++ pixelaria-website/src/main.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from 'react-helmet-async'
-import './index.css'
-import App from './App.tsx'
+import "./index.css";
+import App from "./App";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const Wrapper = import.meta.env.DEV ? React.Fragment : React.StrictMode;
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Wrapper>
     <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </HelmetProvider>
-  </StrictMode>,
-)
+  </Wrapper>
+);
